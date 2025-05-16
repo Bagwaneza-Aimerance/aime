23062ba93b6dd0e14fd3acef63c6ae9c6debf44e
Bagwaneza-Aimerance <bagwanezaimerance5@gmail.com>
2025-05-16 13:22:44 -1000

    update

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$conn=mysqli_connect("localhost","root","","berwashop");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+else
+{
+    echo "Connected successfully bagwaneza aimerance";
+}
+
+
+
+
+
+
+?>
